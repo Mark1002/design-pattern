b929d023b12f818145854ee74a9d5ad8dc849505
Mark Chang <ak0922747401@gmail.com>
2018-07-08 18:38:04 +0800

    update some commit

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-# python design pattern practice
+## python design pattern practice
+
+新增 singleton pattern at 2018-07-08
--- singleton/singleton_meta.py
+++ singleton/singleton_meta.py
@@ -2,7 +2,7 @@ class SingletonMetaclass(type):
     def __init__(self, *args, **kwargs):
         self.__instance = None
         super().__init__(*args, **kwargs)
-    # __call__ 表示這個類別實例可以當成函數呼叫
+    # 決定若使用類別來建構物件時，該如何進行物件的建立與初始
     def __call__(self, *args, **kwargs):
         if self.__instance is None:
             self.__instance = super().__call__(*args, **kwargs)
